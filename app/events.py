5dc546f0119c2ab57bd318e91175743ca37f7929
Bigtoyka <153129647+Bigtoyka@users.noreply.github.com>
2024-03-14 23:09:16 +0300

    probabilit and events

--- app/events.py
+++ app/events.py
@@ -1,16 +1,16 @@
-Импортируем файл probability
-def two_coins():
-    """"
-    Вычисляем веротяности чего?
-
-    """"
-    # ...
-    ...
+from probability import calculator_probability
+def two_coins(a,b):
+    """
+    Вычисляем веротяность вападение подряд 2 любых исхода
+    """
+    prob_2_coins = calculator_probability("coin",a)**2
+    return prob_2_coins
     
-def two_dise():
-    """"
-    Описание функции здесь
-    """"
-    # ...
-    ...
-    
+def two_dice(a,b):
+    """
+    Вычисляем веротяность вападение подряд 2 любых исхода
+    """
+    prob_2_coins = calculator_probability("dice",a)**2
+    return prob_2_coins
+a = two_dice(1,2)
+print(a) 
