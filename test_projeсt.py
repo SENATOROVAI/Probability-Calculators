0e23767295e46a7d7f2f398c0adef2e61b18372e
zatomaks <mr.maks.pupkin.@mail.ru>
2024-03-14 12:48:07 +0300

    +

---
+++ test_projeсt.py
@@ -0,0 +1,13 @@
+Подключение файла probability
+# Имопрт unittest, если умеешь пользовать
+
+Проверяем код с помощь функции assert python
+
+
+# Example:
+# Test if a condition returns True:
+
+# x = "hello"
+
+# #if condition returns True, then nothing happens:
+# assert x == "hello"
