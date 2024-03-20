07d937efd3959b30da72c524d5c74c50ee9ebf46
zatomaks <mr.maks.pupkin.@mail.ru>
2024-03-20 21:59:11 +0300

    -

--- app/probability.py
+++ app/probability.py
@@ -1,26 +1,27 @@
 from main import get_value
 
+
 def calculator_probability() -> float:
     """
-    Вычисляет вероятность события. 
-     
-    Args: 
+    Вычисляет вероятность события.
+
+    Args:
         event_type: Тип события(монетка, кубик, ваш текст здесь)
         event: событие(сторона монетки, число на кубике, ваш текст здесь)
-    Return: 
+    Return:
         Веротность события
     """
-    
-    
-    event_type, event  = get_value()
-    if event_type == "coin": 
-        prob = 1/2 * 100
-        return f'{prob}%'
-    elif event_type == "dice":
-        return f'{round((1 / 6) * 100)}%'
 
-#print(calculator_probability())
-    #  ваш код дальнейшей реализации здесь???
-    
-    # иначе: ???
-    #     райс велью еррор (неверный тип события)??
+    event_type, event = get_value()
+    if event_type == "coin":
+        prob = 1 / 2 * 100
+        return f"{prob}%"
+    elif event_type == "dice":
+        return f"{round((1 / 6) * 100, 1)}%"
+
+
+# print(calculator_probability())
+#  ваш код дальнейшей реализации здесь???
+
+# иначе: ???
+#     райс велью еррор (неверный тип события)??
