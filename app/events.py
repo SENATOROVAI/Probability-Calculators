29c501145b027166ef5087e1e1b55b3740fd447a
zatomaks <mr.maks.pupkin.@mail.ru>
2024-03-21 18:39:42 +0300

    +

--- app/events.py
+++ app/events.py
@@ -1,18 +1,21 @@
 from probability import calculator_probability
-def two_coins(a,b):
+
+
+def two_coins(a, b):
     """
     Вычисляем веротяность вападение подряд b любых исхода
     a = орел или решка 0, орел, 1 - решка
     b = кол-во выпадений
     """
-    prob_2_coins = calculator_probability("coin",a)**b
+    prob_2_coins = calculator_probability("coin", a) ** b
     return prob_2_coins
-    
-def two_dice(a,b):
+
+
+def two_dice(a, b):
     """
     Вычисляем веротяность вападение подряд b любых исхода
     a = число на кубике 1,2,3,4,5,6
     b = кол-во выпадений
     """
-    prob_2_coins = calculator_probability("dice",a)**b
+    prob_2_coins = calculator_probability("dice", a) ** b
     return prob_2_coins
--- app/main.py
+++ app/main.py
@@ -1,39 +1,43 @@
 # одключаем ядро
 
+
 # Прописание инпуты:
 def get_value():
     """
     Принимает занчения
-    
-    Выбор типа события 
+
+    Выбор типа события
     """
     event_type = int(input("Выберите тип события:\n1 - coin\n2 - dice\n"))
-    while event_type != 1 or event_type !=2: 
+    while event_type != 1 or event_type != 2:
         if event_type == 1:
-            type =  'coin'
+            type = "coin"
             break
 
         elif event_type == 2:
-            type =  'dice'
+            type = "dice"
             break
 
         else:
-            event_type =  int(input('Повторите попытку ввода значения:\n1 - coin\n2 - dice\n'))
-    
+            event_type = int(
+                input("Повторите попытку ввода значения:\n1 - coin\n2 - dice\n")
+            )
+
     if event_type == 1:
-        event = int(input('Выбирите сторону монетки:\n1 - O\n2 - P\n'))
+        event = int(input("Выбирите сторону монетки:\n1 - O\n2 - P\n"))
         while event < 1 or event > 2:
-            event = int(input('Повторите выбор стороны:\n1 - O\n2 - P\n'))
-        return type, {2: 'P', 1: 'O'}[event]
+            event = int(input("Повторите выбор стороны:\n1 - O\n2 - P\n"))
+        return type, {2: "P", 1: "O"}[event]
     else:
-        event = int(input('Выбирите число на кубике:\nОт 1 до 6\n'))
+        event = int(input("Выбирите число на кубике:\nОт 1 до 6\n"))
         while event > 6 or event < 1:
-            event = int(input('Повторите выбор числа на кубике:\nОт 1 до 6\n'))
+            event = int(input("Повторите выбор числа на кубике:\nОт 1 до 6\n"))
         return type, event
+
+
 # def get():
 #     b = get_value()
-#     return b   
+#     return b
 # print(get())
 # print(get_value())
 # старт проекта(вызов функций)
-
