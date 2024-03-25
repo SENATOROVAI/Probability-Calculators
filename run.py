063c03f7d98802165e47533ae6025f81938570ea
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2024-03-26 00:07:22 +0300

    Update run.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- run.py
+++ run.py
@@ -2,6 +2,5 @@ from app import main
 
 match __name__:
     case "__main__":
-        type, event = main()
-        result = calculator_probability(type)
-        print(result)
+        main()
+        
